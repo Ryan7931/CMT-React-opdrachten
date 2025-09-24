2f7d21a8a5b861579c951af3f8fedacf1f74787d
Ryan7931 <9024915@student.zadkine.nl>
2025-09-24 12:20:50 +0200

    books

--- opdracht-01-books/src/App.jsx
+++ opdracht-01-books/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Book from './Book.jsx'
 
 function App() {
 
   return (
     <>
       <h1>Opdracht 1 - Books</h1>
+      <Book title="het leven van een loser" author="Ryan"/>
     </>
   )
 }
---
+++ opdracht-01-books/src/Book.jsx
@@ -0,0 +1,10 @@
+const Book = ( { title, author } ) => {
+    return (
+        <section>
+    <h2>{title}</h2>
+    <h3>{author}</h3>
+    </section>
+    );
+}
+
+export default Book;
