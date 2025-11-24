1e52a1785d62ce047659b27f4c3c4b8b8f38ac52
Ryan7931 <9024915@student.zadkine.nl>
2025-11-24 15:00:39 +0100

    twitter

--- opdracht-07-twitter/src/App.jsx
+++ opdracht-07-twitter/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import CreateMessage from './components/CreateMessage'
+import MessageList from './components/MessageList'
 
 function App() {
-
+  let name = "john";
+  let message = "Hallo ik ben john";
   return (
     <>
       <h1>Opdracht 7 - Twitter</h1>
+      <CreateMessage />
+      <MessageList name={name} message={message}/>
     </>
   )
 }
---
+++ opdracht-07-twitter/src/components/CreateMessage.jsx
@@ -0,0 +1,10 @@
+const CreateMessage = () => {
+    return ( 
+     <form action="">
+        <textarea cols={50} rows={5}></textarea>
+        <button>Toevoegen</button>
+     </form>
+     );
+}
+ 
+export default CreateMessage;
