ea03fc444310dac07aa89d59a134c9388b5b08e0
Ryan7931 <9024915@student.zadkine.nl>
2025-11-26 14:08:31 +0100

    opdracht 9

--- opdracht-09-tailwind/src/App.jsx
+++ opdracht-09-tailwind/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import Logo from './assets/logo-workcation.png'
+import MainPhoto from './assets/DameOpVakantie.png'
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-center text-red-500'>Opdracht 9 - Tailwind</h1>
-    </>
+    <section className="bg-gray-100">
+    <section className='w-160 place-self-center '>
+    <div className='flex place-self-center my-8 gap-5'>
+      <img className='w-25 place-self-center' src= {Logo} />
+      <h1 className='text-center text-black my-8 text-6xl font-bold'>Work<span className='text-purple-600' >Cation</span></h1>
+    </div>
+    <img className='place-self-center rounded-2xl shadow-2xl' src= {MainPhoto} />
+    <h2 className='text-center text-7xl font-bold my-8'>You can work from anywhere. <span className='text-purple-600'>Take advantage of it.</span></h2>
+    <p className='my-4 text-3xl'>WorkCation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when you're on vacation.</p>
+    <button className='bg-purple-600 my-8 text-white p-5 w-70 rounded-2xl text-2xl'>BOOK YOUR ESCAPE</button>
+   </section>
+    </section>
   )
 }
 
-export default App
+export default App;
