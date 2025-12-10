f65fedac1aed1273577255658c19361e058b423d
Ryan7931 <9024915@student.zadkine.nl>
2025-12-10 12:18:34 +0100

    opdracht 13.1

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Book = ({ id, title, author, image, category }) => {
   const navigate = useNavigate();
+
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = (e) => {
+    e.stopPropagation(); 
+    setLiked(!liked);
+  };
+
   return (
-    <section className="book-card" onClick={() => navigate(`/book/${id}`)} style={{cursor:'pointer'}}>
+    <section
+      className="book-card"
+      onClick={() => navigate(`/book/${id}`)}
+      style={{ cursor: 'pointer' }}
+    >
       <div className="book-img-wrap">
         <img src={image} alt={title} className="book-img" />
       </div>
       <div className="book-info">
         <h2 className="book-title">{title}</h2>
         <h3 className="book-author">{author}</h3>
-
         <p className="book-category">Categorie: {category}</p>
+
+        <div className="favorite-section">
+          <button onClick={toggleLike} className="heart-button">
+            {liked ? '❤️' : '🤍'}
+          </button>
+          {liked && <p className="favorited-text">Toegevoegd aan favorieten</p>}
+        </div>
       </div>
     </section>
   );
