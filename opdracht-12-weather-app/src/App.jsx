539b3c94e91aa0c28f21c2b338f86b3f14dde1d4
Ryan7931 <9024915@student.zadkine.nl>
2025-12-10 12:07:14 +0100

    weather app

--- opdracht-12-weather-app/src/App.jsx
+++ opdracht-12-weather-app/src/App.jsx
@@ -1,12 +1,39 @@
-import './App.css'
+import { useState } from "react";
+import WeatherInfo from "./components/WeatherInfo";
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  // >>> HIERZETJEJAPIKEY <<<
+  const apiKey = "d16f14e2a243a52cce06bac7ed949d2b";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+
+    const data = await response.json();
+    setWeatherData(data);
+  };
 
   return (
-    <>
-      <h1>Opdracht 12 - Weather App</h1>
-    </>
-  )
-}
+    <div className="max-w-md mx-auto mt-10 p-4 border rounded shadow">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Voer een stad in"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit">Zoeken</button>
+      </form>
 
-export default App
+      {weatherData && <WeatherInfo data={weatherData} />}
+    </div>
+  );
+};
+
+export default App;
---
+++ opdracht-12-weather-app/src/components/WeatherInfo.jsx
@@ -0,0 +1,23 @@
+const WeatherInfo = ({ data }) => {
+  const icon = data.weather[0].icon;
+
+  return (
+    <div className="bg-white p-4 rounded shadow text-center text-3xl">
+
+      <h2 className="text-4xl font-bold mb-2">{data.name}</h2>
+
+      <img
+        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+        alt="weer icoon"
+        className="mx-auto"
+      />
+
+      <p className="text-xl">🌡️ {data.main.temp}°C</p>
+      <p>Gevoelstemperatuur: {data.main.feels_like}°C</p>
+      <p>Luchtvochtigheid: {data.main.humidity}%</p>
+      <p>Wind: {data.wind.speed} m/s</p>
+    </div>
+  );
+};
+
+export default WeatherInfo;
